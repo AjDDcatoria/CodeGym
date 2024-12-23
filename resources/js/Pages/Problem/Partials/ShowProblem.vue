0c090a9b681e53ec950618e756b28efa89abc62a
AJ DDcatoria <ajdedicatoria9@gmail.com>
2024-12-23 14:15:57 +0800

    fixed merge conflicts

--- resources/js/Pages/Problem/Partials/ShowProblem.vue
+++ resources/js/Pages/Problem/Partials/ShowProblem.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import { Splitpanes, Pane } from 'splitpanes';
-import 'splitpanes/dist/splitpanes.css';
+import TopBarEditor from "@/Components/Editor/TopBarEditor.vue";
 import CodeEditor from "@/Components/Editor/CodeEditor.vue";
-import TopBar from "@/Components/Editor/TopBarEditor.vue";
 import OutputTopbar from "@/Components/Editor/OutputTopbar.vue";
+import 'splitpanes/dist/splitpanes.css'
+import { Splitpanes, Pane } from 'splitpanes'
+
 </script>
 
 <template>
@@ -11,16 +12,16 @@ import OutputTopbar from "@/Components/Editor/OutputTopbar.vue";
         <header class="h-16 flex items-center px-2">Test Header</header>
         <Splitpanes class="gap-1">
             <Pane class="pane p-3" :size="50" :min-size="25">
-                <div>Description</div>
+                <div>Problem</div>
             </Pane>
             <Pane :size="50" :min-size="25">
                 <Splitpanes horizontal class="gap-1">
-                    <Pane class="pane" :size="80" :min-size="20">
-                        <TopBar />
+                    <Pane class="pane" :size="70" :min-size="20">
+                        <TopBarEditor/>
                         <CodeEditor />
                     </Pane>
-                    <Pane class="pane overflow-hidden" :size="20" :min-size="20">
-                       <OutputTopbar />
+                    <Pane class="pane p-3" :size="30" :min-size="25">
+                        <OutputTopbar />
                     </Pane>
                 </Splitpanes>
             </Pane>
@@ -30,8 +31,8 @@ import OutputTopbar from "@/Components/Editor/OutputTopbar.vue";
 
 <style scoped>
 .pane {
-    background-color:var(--card-background);
-    border-radius: 0.5rem;
-    border: 1px solid  rgb(209 213 219 / 0.2);
+    background:var(--card-background);
+    border-radius:0.5rem;
+    border-color: rgb(209 213 219 / 0.2);
 }
 </style>
